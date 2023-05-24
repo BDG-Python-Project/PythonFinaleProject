b78b89244c3641a2fd3e0116d81600dc79a91a3c
Terok92 <tereza.mid@gmail.com>
2023-05-25 02:33:02 +0400

    Fixed some unnecessary parts

--- character.py
+++ character.py
@@ -3,5 +3,11 @@ class Base:
         self.name = name
         self.age = age
 
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age
+
     def say_hello(self, person):
         return "Բարև, " + person.get_name()
--- fool.py
+++ fool.py
@@ -5,11 +5,5 @@ class Fool(Base):
     def __init__(self, name, age):
         Base.__init__(self, name, age)
 
-    def get_name(self):
-        return self.name
-
-    def get_age(self):
-        return self.age
-
     def say_hello(self, person):
         return self.get_name() + ":\n\tԲարև, " + person.get_name() + "!"
